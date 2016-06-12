0e1d57d283344c5f787ededee553ffd3063d84e5
Samuel Lijin <samuel.lijin@formlabs.com>
2016-06-11 23:19:46 -0400

    renamed run.sh to init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
-# Runs the appropriate commands for this .vimrc.
+# Runs the appropriate commands to set up this .vimrc.
 
 git submodule update --init --recursive
 sudo apt install vim-python-jedi
+ln -s .vimrc ~/.vimrc
