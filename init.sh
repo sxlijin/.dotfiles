98cdd85616579e0543a87b31caa7bed78c18b41f
Samuel Lijin <samuel.lijin@formlabs.com>
2016-07-09 20:32:52 -0400

    stop adding recursive symlink on re-init.sh

--- init.sh
+++ init.sh
@@ -6,8 +6,10 @@ git submodule update --init --recursive
 sudo apt install vim-python-jedi
 
 dotfiles_dir=$(git rev-parse --show-toplevel)
-ln -s ${dotfiles_dir}/vim ~/.vim
-ln -s .vim/vimrc ~/.vimrc
+if [ ! -d ~/.vim/ ]; then
+    ln -s ${dotfiles_dir}/vim ~/.vim/
+    ln -s .vim/vimrc ~/.vimrc
+fi
 ln -s ${dotfiles_dir}/bash_aliases ~/.bash_aliases
 ln -s ${dotfiles_dir}/screenrc ~/.screenrc
 ln -s ${dotfiles_dir}/inputrc ~/.inputrc
