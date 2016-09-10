0c7dc37cf9154a0237625b9212a04ff4959302f5
Samuel Lijin <sxlijin+github@gmail.com>
2016-09-10 09:27:04 +0000

    bring update.sh in line with new additions

--- update.sh
+++ update.sh
@@ -1,5 +1,19 @@
-#! /bin/bash
+#!/bin/bash
 
 echo "Running custom updates..."
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o git/git-completion.bash
-git submodule update --remote
+
+echo "# automatically generated using dircolors(1) via --print-database" > dircolors && \
+echo "#" >> dircolors && \
+dircolors --print-database >> dircolors \
+    && echo "Updated dircolors"
+
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash \
+    -o git/git-completion.bash \
+    && echo "Updated git/git-completion.bash"
+
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh \
+    -o git/git-prompt.sh \
+    && echo "Updated git/git-prompt.sh"
+
+git submodule update --remote \
+    && echo "Updated git submodules"
