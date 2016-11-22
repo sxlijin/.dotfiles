46080c16cf29eb93b225cdaa645be0c3a23ab35b
Samuel Lijin <sxlijin@gmail.com>
2016-11-22 16:26:15 -0600

    fix init.sh symlinking

--- init.sh
+++ init.sh
@@ -11,15 +11,22 @@ if [ ! -d ~/.vim/ ]; then
     ln -s .vim/vimrc ~/.vimrc
 fi
 
-# symlink the bash/ config files
-ln -s "${dotfiles_dir}/bash/profile" ~/.bash_profile
-ln -s "${dotfiles_dir}/bash/rc" ~/.bashrc
-ln -s "${dotfiles_dir}/bash/aliases" ~/.bash_aliases
+while read link_dest link_ptr
+do
+    link_ptr="${HOME}/${link_ptr}"
 
-# symlink other files
-ln -s "${dotfiles_dir}/screenrc" ~/.screenrc
-ln -s "${dotfiles_dir}/inputrc" ~/.inputrc
-ln -s "${dotfiles_dir}/git/ignore" ~/.gitignore
+    test -f "${link_ptr}" \
+        && echo "file exists: ${link_ptr}" \
+        && mv "${link_ptr}" "${link_ptr}.bak"
+    ln -s "${link_dest}" "${link_ptr}"
+done << HERE_DOC
+${dotfiles_dir}/bash/profile    .bash_profile
+${dotfiles_dir}/bash/rc         .bashrc
+${dotfiles_dir}/bash/aliases    .bash_aliases
+${dotfiles_dir}/screenrc        .screenrc
+${dotfiles_dir}/inputrc         .inputrc
+${dotfiles_dir}/git/ignore      .gitignore
+HERE_DOC
 
 # don't symlink this: global git config will include machine-specific config
 git config --global include.path ${dotfiles_dir}/git/config
