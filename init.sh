618fe3a21a48fe1d7bdaf0de5af54ca01939604f
Samuel Lijin <sxlijin@google.com>
2017-12-31 01:14:55 -0800

    misc: name files more explicitly, correspondingly fix init.sh

--- init.sh
+++ init.sh
@@ -11,26 +11,26 @@ tstamp="${tstamp//:/-}"
 
 while read link_dest link_ptr
 do
-    link_ptr="${HOME}/${link_ptr}"
+  link_ptr="${HOME}/${link_ptr}"
 
-    test -e "${link_ptr}" \
-        && echo "[WARN] file/dir exists: ${link_ptr}" \
-        && mv "${link_ptr}" "${link_ptr}_${tstamp}.bak"
+  (test -L "${link_ptr}" || test -e "${link_ptr}") \
+    && echo "[WARN] file/dir exists: ${link_ptr}" \
+    && mv "${link_ptr}" "${link_ptr}_${tstamp}.bak"
 
-    ln -s "${link_dest}" "${link_ptr}"
+  ln -s "${link_dest}" "${link_ptr}"
 
 done << HERE_DOC
-${dotfiles_dir}/bash/profile    .bash_profile
-${dotfiles_dir}/bash/rc         .bashrc
-${dotfiles_dir}/bash/aliases    .bash_aliases
-${dotfiles_dir}/bash/locals     .bash_locals
-${dotfiles_dir}/git/ignore      .gitignore
-${dotfiles_dir}/inputrc         .inputrc
-${dotfiles_dir}/screenrc        .screenrc
-${dotfiles_dir}/tmux.conf       .tmux.conf
-${dotfiles_dir}/vim             .vim
-.vim/vimrc                      .vimrc
+${dotfiles_dir}/bash/bash_profile .bash_profile
+${dotfiles_dir}/bash/bashrc       .bashrc
+${dotfiles_dir}/bash/bash_aliases .bash_aliases
+${dotfiles_dir}/git/gitignore     .gitignore
+${dotfiles_dir}/inputrc           .inputrc
+${dotfiles_dir}/screenrc          .screenrc
+${dotfiles_dir}/tmux.conf         .tmux.conf
+${dotfiles_dir}/vim               .vim
+.vim/vimrc                        .vimrc
+.vim/gvimrc                       .gvimrc
 HERE_DOC
 
 # don't symlink this: global git config will include machine-specific config
-git config --global include.path ${dotfiles_dir}/git/config
+git config --global include.path ${dotfiles_dir}/git/gitconfig
